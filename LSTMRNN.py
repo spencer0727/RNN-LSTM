b2cf0734c16bcd39a75fdfe3c41d4d66dc6ea8f9
Spencer Frebel <sfrebel@bacelineinvestments.com>
2018-12-11 11:19:20 +0100

    Update LSTMRNN.py

--- LSTMRNN.py
+++ LSTMRNN.py
@@ -15,6 +15,18 @@ scaler = StandardScaler()
 
 scaled_dataset = scaler.fit_transform(data_to_use.reshape(-1, 1))
 
+
+'''
+    This function is used to create Features and Labels datasets. By windowing the data.
+    
+    Input: data - dataset used in the project
+           window_size - how many data points we are going to use to predict the next datapoint in the sequence 
+                       [Example: if window_size = 1 we are going to use only the previous day to predict todays stock prices]
+    
+    Outputs: X - features splitted into windows of datapoints (if window_size = 1, X = [len(data)-1, 1])
+             y - 'labels', actually this is the next number in the sequence, this number we are trying to predict
+    '''
+
 def window_data(data, window_size):
     X = []
     y = []
